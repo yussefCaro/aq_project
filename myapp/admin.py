3625e58e6e212605b0e664ab6d022627585a857f
Yussef Caro <yussefcaro@gmail.com>
2025-01-21 15:32:02 -0500

    ceacion tareas modulo admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Task, Project
+
+class   TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
 # Register your models here.
-admin.site.register(Task)
+
+
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Project)
--- myapp/models.py
+++ myapp/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
-        return self.name
+        return str (self.name)
 
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250,  blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField (null =True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE )
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    done = models.BooleanField(default=False)
+    
 
     def __str__(self):
-        return self.title + ' - ' + self.project.name
+        return self.title + ' - by ' + self.user.username
